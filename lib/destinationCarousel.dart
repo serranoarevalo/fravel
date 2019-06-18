b577560a97f3342b3ce6cdbe4e595dc533621c83
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-06-18 13:16:56 +0900

    Destination Card with Overlay

--- lib/destinationCarousel.dart
+++ lib/destinationCarousel.dart
@@ -12,9 +12,9 @@ class DestinationCarousel extends StatelessWidget {
   final List<Destination> destinations = [
     Destination("Antelope Canyon", "Yesterday", 4.7,
         "https://images.unsplash.com/photo-1560750588-73207b1ef5b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"),
-    Destination("Kamchatka Peninsula", "A week ago", 5,
+    Destination("Kamchatka Peninsula", "A week ago", 4.9,
         "https://images.unsplash.com/photo-1560780551-bd5d3eacd2c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"),
-    Destination("Genteng Lembang", "Two Months Ago", 5,
+    Destination("Genteng Lembang", "Two Months Ago", 4.3,
         "https://images.unsplash.com/photo-1560713274-b1522b37f88b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"),
   ];
 
@@ -50,15 +50,43 @@ class DestinationCard extends StatelessWidget {
         margin: EdgeInsets.only(right: 10),
         padding: EdgeInsets.all(20),
         width: 150,
-        alignment: Alignment.bottomLeft,
         decoration: BoxDecoration(
+            color: Colors.black,
             borderRadius: BorderRadius.all(Radius.circular(10)),
             image: DecorationImage(
-                fit: BoxFit.cover, image: NetworkImage(data.photoUrl))),
-        child: Text(data.title,
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.w600)));
+                colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.7), BlendMode.dstATop),
+                fit: BoxFit.cover,
+                image: NetworkImage(data.photoUrl))),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    data.rating.toString(),
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  )
+                ],
+              ),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.only(bottom: 7),
+                        child: Text(data.title,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ))),
+                    Container(
+                      child: Text(
+                        data.date,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ])
+            ]));
   }
 }
